4adb087cf11cb43841daa78c1c4d03f6991aec2b
Travis CI User <travis@example.org>
2021-07-01 12:35:35 +0000

    Merge pull request #5 from gkarat/redux-setup
    Draft: Configure Redux store, link app with Smart Proxy API

--- js/OopsPage.3692239ceb730e048419.js
+++ js/OopsPage.3692239ceb730e048419.js
@@ -1,2 +1,2 @@
 (self.webpackChunkocp_advisor_frontend=self.webpackChunkocp_advisor_frontend||[]).push([[196],{4947:(e,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>a});var l=t(3264),o=t.n(l),u=t(6530),c=t(6350),r=t(167);const a=(0,u.withRouter)((function(){return(0,l.useEffect)((function(){var e,n,t;null===(e=insights)||void 0===e||null===(n=e.chrome)||void 0===n||null===(t=n.appAction)||void 0===t||t.call(n,"oops-page")}),[]),o().createElement(c.Z,null,o().createElement(r.Z,null))}))}}]);
-//# sourceMappingURL=../sourcemaps/OopsPage.24e564f75049917ccdbc.js.map
+//# sourceMappingURL=../sourcemaps/OopsPage.7ffebb1681f8cf4ce209.js.map
