4b145b9226e30406b199a685e106c13e88918133
Travis CI User <travis@example.org>
2021-10-28 11:33:36 +0000

    Release to prod/beta (#48)
    * Pass impact number to  rule details component
    
    * Recommendations list table, initial (#39)
    
    * Create the Recommendations list components and table
    
    * Fix minor bugs found during development
    
    * Hide toolbar when zero hits and change empty state title (#43)
    
    * Hide toolbar and chips when 0 recs
    
    * Fix no recs title for the single cluster page
    
    * Fix invalid link in recommendation row (#44)
    
    * Separate 404 empty state for cluster details (#42)
    
    Co-authored-by: Aleksandr Voznesenskii <avoznese@redhat.com>
    
    * Error boundary component (#45)
    
    Wraps the whole application with custom error boundary
    
    Co-authored-by: Aleksandr Voznesenskii <avoznese@redhat.com>
    
    * Redirect from the root to the recommendations page (#46)
    
    * Fetch both impacting and non-impacting recs (#47)
    
    * Added first Cypress tests (#36)
    
    Cypress tests first version; package.json and ESLint config update
    
    * Fix cypress test failure; update scripts and README (#49)
    
    * Update intercepted endpoint after the recent API change
    
    * Update package.json scripts and README.md
    
    Co-authored-by: Aleksandr Voznesenskii <avoznese@redhat.com>
    Co-authored-by: Dmitrii Misharov <dmisharo@redhat.com>

---
+++ js/661.2eb1e5e7517d5abf92c5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkocp_advisor_frontend=self.webpackChunkocp_advisor_frontend||[]).push([[661],{52661:(e,n,r)=>{r.d(n,{Z:()=>u});var t=r(2095),a=r(85962),l=r(24561),c=r(93264),o=r.n(c);const u=function(){return o().createElement(t.Card,null,o().createElement(a.CardBody,null,o().createElement(l.aV,null)))}}}]);
