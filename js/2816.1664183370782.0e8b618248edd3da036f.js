87673bcb6d6afaf247c8802fb146039244a9b2a2
Travis CI User <travis@example.org>
2022-09-27 08:32:17 +0000

    chore(deps): bump @patternfly/react-table from 4.104.7 to 4.108.0 (#422)
    Bumps [@patternfly/react-table](https://github.com/patternfly/patternfly-react) from 4.104.7 to 4.108.0.
    - [Release notes](https://github.com/patternfly/patternfly-react/releases)
    - [Commits](https://github.com/patternfly/patternfly-react/compare/@patternfly/react-table@4.104.7...@patternfly/react-table@4.108.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@patternfly/react-table"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- js/2816.1664183370782.0e8b618248edd3da036f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkocp_advisor_frontend=self.webpackChunkocp_advisor_frontend||[]).push([[2816],{72816:()=>{}}]);
--- js/497.1664183370782.0e8b618248edd3da036f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkocp_advisor_frontend=self.webpackChunkocp_advisor_frontend||[]).push([[497],{10497:()=>{}}]);
